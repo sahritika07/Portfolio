3cbccda59ba69ec5eacc5d9be87ef0af5edb3b76
Ritika Sah <ritikasah731@gmail.com>
2025-05-04 11:11:26 +0530

    Changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,18 +9,17 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify"  }}>
             Hi Everyone, I am <span className="purple">Ritika Sah </span>
-            from <span className="purple"> Ranchi,Jharkhand.</span> </p>
+            from <span className="purple"> Ranchi,Jharkhand.</span> <br/>
+            I am currently employed as a software developer at Aadrika Enterprises. </p>
             <br />
-            <p>
-            I am currently employed as a software developer at Aadrika Enterprises.
+        
+            <p style={{ textAlign: "justify"  }}>
+           
+            I have completed Graduation in Bachelor's of Computer Application from Ranchi University. I scored 86% in my Graduation . 
+            <br />
             </p>
             <br />
-            <p>
-            I have completed Graduation in Bachelor's of Computer Application from Ranchi University. I scored 86% in my Graduation
-            <br />
-            </p>
-            <br />
-            <p>
+            <p style={{ textAlign: "justify"  }}>
             Apart from coding, some other activities that I love to do!
             </p>
          
